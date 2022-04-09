0fb0c87e7418cd910774ae6dbe2b3afd3dc540dc
neelJadav27 <85033138+neelJadav27@users.noreply.github.com>
2022-04-09 00:07:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,158 +1,152 @@
-//import { items } from "./menu";
-// console.log(items);
-
-//import "./styles.css";
-
-// const items = [
-//   {
-//     name: "Meat Lasagna",
-//     description:
-//       "Fresh baked lasagna layered with ground beef, ricotta, mozzarella and house made tomato sauce",
-//     type: "Pasta",
-//     // imageURL: "../assets/Calamari.jpg",
-//     imageURL: "./assets/MeatLasagna.jpg",
-//     price: 9.99,
-//     spicy: false,
-//     menuOrder: 0,
-//   },
-//   {
-//     name: "Calamari",
-//     description:
-//       "Tender squid rings fried in a light seasoning, served with balsamic mayo dip",
-//     type: "Starters",
-//     imageURL: "./assets/Calamari.jpg",
-//     price: 8.5,
-//     spicy: false,
-//     menuOrder: 1,
-//   },
-//   {
-//     name: "Margherita",
-//     description:
-//       "Fresh sliced mozzarella with a special plum tomato sauce and fresh basil",
-//     type: "Pizza",
-//     imageURL: "./assets/MargaritaPizza.jpg",
-//     price: 15.95,
-//     spicy: false,
-//     menuOrder: 1,
-//   },
-//   {
-//     name: "Baked Manicotti",
-//     description:
-//       "Filled with ricotta cheese and topped with romano, mozzarella and a side of pasta",
-//     type: "Pasta",
-//     imageURL: "./assets/manicotti.jpg",
-//     price: 9.99,
-//     spicy: false,
-//     menuOrder: 3,
-//   },
-//   {
-//     name: "Chicken Parmigiana",
-//     description:
-//       "Freshed breaded chicken breast with house made tomato sauce, romano, mozzarella cheese and a side of pasta",
-//     type: "Pasta",
-//     imageURL: "./assets/chickenParmigian.jpg",
-//     price: 10.29,
-//     spicy: false,
-//     menuOrder: 6,
-//   },
-//   {
-//     name: "Eggplant Parmigiana",
-//     description:
-//       "Layers of fresh battered eggplant with house made tomato sauce, romano, mozzarella chees and a side of pasta",
-//     type: "Pasta",
-//     imageURL: "./assets/Eggplant.jpg",
-//     price: 8.99,
-//     spicy: false,
-//     menuOrder: 5,
-//   },
-//   {
-//     name: "Spinach Pizza",
-//     description: "Garlic pizza with spinach, ricotta and mozzarella",
-//     type: "Pizza",
-//     imageURL: "./assets/SpinachPizzza.jpg",
-//     price: 16.5,
-//     spicy: false,
-//     menuOrder: 2,
-//   },
-//   {
-//     name: "Grandma's Pizza",
-//     description:
-//       "Sicilian pizza crust with fresh sliced mozzarella, marinara sauce, fresh basil and olive oil",
-//     type: "Pizza",
-//     imageURL: "./assets/grandmaPizza.jpg",
-//     price: 19.95,
-//     spicy: false,
-//     menuOrder: 3,
-//   },
-//   {
-//     name: "Soup of the Day",
-//     description: "Please ask your server",
-//     type: "Starters",
-//     imageURL: "./assets/Soup.jpg",
-//     price: 6.5,
-//     spicy: false,
-//     menuOrder: 2,
-//   },
-//   {
-//     name: "Standard",
-//     description:
-//       "White pizza with fresh sliced mozzarella, prosciutto, white sauce, fresh basil and olive oil",
-//     type: "Pizza",
-//     imageURL: "./assets/Standard.jpg",
-//     price: 19.95,
-//     spicy: true,
-//     menuOrder: 0,
-//   },
-//   {
-//     name: "Baked Ziti",
-//     description: "With tomato sauce and mozzarella",
-//     type: "Pasta",
-//     price: 8.99,
-//     imageURL: "./assets/baked ziti.jpg",
-//     spicy: false,
-//     menuOrder: 4,
-//   },
-//   {
-//     name: "Baked Ravioli",
-//     description:
-//       "Filled with ricotta cheese and topped with romano, mozzarella and house made tomato sauce",
-//     type: "Pasta",
-//     imageURL: "./assets/bakedRavioli.jpg",
-//     price: 9.99,
-//     spicy: false,
-//     menuOrder: 2,
-//   },
-//   {
-//     name: "Stuffed Shells",
-//     description:
-//       "Filled with ricotta cheese and topped with romano, mozzarella and house made tomato sauce",
-//     type: "Pasta",
-//     imageURL: "./assets/stuffedShells.jpg",
-//     price: 9.99,
-//     spicy: false,
-//     menuOrder: 1,
-//   },
-//   {
-//     name: "Chicken Wing",
-//     description:
-//       "Shaved breaded chicken with a special blend of hot wing sauce and bleu cheese with mozzarella",
-//     type: "Pizza",
-//     imageURL: "./assets/ChickenWings.jpg",
-//     price: 19.95,
-//     spicy: true,
-//     menuOrder: 4,
-//   },
-//   {
-//     name: "Insalata Caprese",
-//     description:
-//       "A classic Italian salada made with freshly sliced mozzarella, beef tomatoes and fresh basil",
-//     type: "Starters",
-//     imageURL: "./assets/Caprese.jpg",
-//     price: 6.5,
-//     spicy: false,
-//     menuOrder: 0,
-//   },
-// ];
+const items = [
+  {
+    name: "Meat Lasagna",
+    description:
+      "Fresh baked lasagna layered with ground beef, ricotta, mozzarella and house made tomato sauce",
+    type: "Pasta",
+    imageURL: "./assets/MeatLasagna.jpg",
+    price: 9.99,
+    spicy: false,
+    menuOrder: 0,
+  },
+  {
+    name: "Calamari",
+    description:
+      "Tender squid rings fried in a light seasoning, served with balsamic mayo dip",
+    type: "Starters",
+    imageURL: "./assets/Calamari.jpg",
+    price: 8.5,
+    spicy: false,
+    menuOrder: 1,
+  },
+  {
+    name: "Margherita",
+    description:
+      "Fresh sliced mozzarella with a special plum tomato sauce and fresh basil",
+    type: "Pizza",
+    imageURL: "./assets/MargaritaPizza.jpg",
+    price: 15.95,
+    spicy: false,
+    menuOrder: 1,
+  },
+  {
+    name: "Baked Manicotti",
+    description:
+      "Filled with ricotta cheese and topped with romano, mozzarella and a side of pasta",
+    type: "Pasta",
+    imageURL: "./assets/manicotti.jpg",
+    price: 9.99,
+    spicy: false,
+    menuOrder: 3,
+  },
+  {
+    name: "Chicken Parmigiana",
+    description:
+      "Freshed breaded chicken breast with house made tomato sauce, romano, mozzarella cheese and a side of pasta",
+    type: "Pasta",
+    imageURL: "./assets/chickenParmigian.jpg",
+    price: 10.29,
+    spicy: false,
+    menuOrder: 6,
+  },
+  {
+    name: "Eggplant Parmigiana",
+    description:
+      "Layers of fresh battered eggplant with house made tomato sauce, romano, mozzarella chees and a side of pasta",
+    type: "Pasta",
+    imageURL: "./assets/Eggplant.jpg",
+    price: 8.99,
+    spicy: false,
+    menuOrder: 5,
+  },
+  {
+    name: "Spinach Pizza",
+    description: "Garlic pizza with spinach, ricotta and mozzarella",
+    type: "Pizza",
+    imageURL: "./assets/SpinachPizzza.jpg",
+    price: 16.5,
+    spicy: false,
+    menuOrder: 2,
+  },
+  {
+    name: "Grandma's Pizza",
+    description:
+      "Sicilian pizza crust with fresh sliced mozzarella, marinara sauce, fresh basil and olive oil",
+    type: "Pizza",
+    imageURL: "./assets/grandmaPizza.jpg",
+    price: 19.95,
+    spicy: false,
+    menuOrder: 3,
+  },
+  {
+    name: "Soup of the Day",
+    description: "Please ask your server",
+    type: "Starters",
+    imageURL: "./assets/Soup.jpg",
+    price: 6.5,
+    spicy: false,
+    menuOrder: 2,
+  },
+  {
+    name: "Standard",
+    description:
+      "White pizza with fresh sliced mozzarella, prosciutto, white sauce, fresh basil and olive oil",
+    type: "Pizza",
+    imageURL: "./assets/Standard.jpg",
+    price: 19.95,
+    spicy: true,
+    menuOrder: 0,
+  },
+  {
+    name: "Baked Ziti",
+    description: "With tomato sauce and mozzarella",
+    type: "Pasta",
+    price: 8.99,
+    imageURL: "./assets/baked ziti.jpg",
+    spicy: false,
+    menuOrder: 4,
+  },
+  {
+    name: "Baked Ravioli",
+    description:
+      "Filled with ricotta cheese and topped with romano, mozzarella and house made tomato sauce",
+    type: "Pasta",
+    imageURL: "./assets/bakedRavioli.jpg",
+    price: 9.99,
+    spicy: false,
+    menuOrder: 2,
+  },
+  {
+    name: "Stuffed Shells",
+    description:
+      "Filled with ricotta cheese and topped with romano, mozzarella and house made tomato sauce",
+    type: "Pasta",
+    imageURL: "./assets/stuffedShells.jpg",
+    price: 9.99,
+    spicy: false,
+    menuOrder: 1,
+  },
+  {
+    name: "Chicken Wing",
+    description:
+      "Shaved breaded chicken with a special blend of hot wing sauce and bleu cheese with mozzarella",
+    type: "Pizza",
+    imageURL: "./assets/ChickenWings.jpg",
+    price: 19.95,
+    spicy: true,
+    menuOrder: 4,
+  },
+  {
+    name: "Insalata Caprese",
+    description:
+      "A classic Italian salada made with freshly sliced mozzarella, beef tomatoes and fresh basil",
+    type: "Starters",
+    imageURL: "./assets/Caprese.jpg",
+    price: 6.5,
+    spicy: false,
+    menuOrder: 0,
+  },
+];
 
 //sort the array with menuOrder
 items.sort((first, second) => {
